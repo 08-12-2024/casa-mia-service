5fc98cb4d38d8932afaae7f6ca63dd2cc8e0d83b
Casa Mia <casamiasrl2024@gmail.com>
2026-05-14 17:44:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-self.addEventListener("fetch", function(event) {});
+const CACHE_NAME = "casamia-v2";
+
+self.addEventListener("install", event => {
+self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => caches.delete(key))
+)
+)
+);
+});
+
+self.addEventListener("fetch", event => {});
